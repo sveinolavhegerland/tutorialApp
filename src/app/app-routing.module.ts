b46872cd6dc85f01f093b3a08c3ffaf31979f010
Svein Olav Hegerland <sveinolavheg@gmail.com>
2020-01-19 15:17:19 +0100

    adding children routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,12 +11,27 @@ import { ChallengeTabsComponent } from "./challenges/challenge-tabs/challenge-ta
 const routes: Routes = [
     { path:'', component: AuthComponent },
 
-    { path: 'challenge-edit', component:ChallengeEditComponent},
-    { path: 'challenges', component:ChallengeTabsComponent, children: [
-        { path: 'today', component:TodayComponent, outlet: 'today'},
-        { path: 'current-challenge', component: CurrentChallengeComponent, outlet: 'currentChallenge'},
-    ]}
-]
+
+    {
+        path: 'challenges',
+
+        children: [
+          { path: 'tabs',
+          component:ChallengeTabsComponent,
+          children: [
+            { path: 'today', component:TodayComponent, outlet: 'today'},
+            {
+              path: 'current-challenge',
+              component: CurrentChallengeComponent,
+              outlet: 'currentChallenge'
+            },
+          ]},
+          { path: ':mode', component:ChallengeEditComponent},
+          { path: '', redirectTo: '/challenges/tabs', pathMatch: 'full'}
+
+        ]
+    }
+];
 
 @NgModule({
     imports: [NativeScriptRouterModule.forRoot(routes)],
--- src/app/challenges/challenge-edit/challenge-edit.component.ts
+++ src/app/challenges/challenge-edit/challenge-edit.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PageRoute } from 'nativescript-angular/router';
 
 @Component({
   selector: 'ns-challenge-edit',
   templateUrl: './challenge-edit.component.html',
   styleUrls: ['./challenge-edit.component.css']
 })
-export class ChallengeEditComponent {
+export class ChallengeEditComponent implements OnInit {
+    isCreating = true;
 
+    constructor(private activatedRoutes: ActivatedRoute, private pageRoute: PageRoute){}
+
+    ngOnInit(){
+        // this.activatedRoutes.paramMap.subscribe(paramMap => {
+        //     console.log(paramMap.get('mode'))
+        // })
+        this.pageRoute.activatedRoute.subscribe(activatedRoute => {
+            activatedRoute.paramMap.subscribe(paramMap => {
+                if (!paramMap.has('mode')){
+                    this.isCreating = true;
+                } else {
+                    this.isCreating = paramMap.get('mode') !== 'edit';
+                }
+            });
+        });
+    }
 
 }
